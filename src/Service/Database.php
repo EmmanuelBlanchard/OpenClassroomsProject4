f14fadcbfa92d5542cc1ad557f0df53e7d205046
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2020-07-26 17:04:20 +0200

    changement du nom de la base de données dans la classe Database.php

--- src/Service/Database.php
+++ src/Service/Database.php
@@ -6,12 +6,12 @@ class Database
 {
     /**
      *  Retourne une connexion à la base de données
-     *
+     * 
      * @return PDO
      */
-    public function getPdo(): PDO
+    public function getPdo(): PDO 
     {
-        $pdo = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'studentBlogPOO', 'zkollf9ITvoJeupP', [
+        $pdo = new PDO('mysql:host=localhost;dbname=openclassrooms_project4;charset=utf8', 'studentOCProject4', 'a#V%tOlefW!*qY&m?eR-$=ZnBw', [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]);
