b606216bf5f852a5d6fee65d4fda427b4114f863
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2020-07-26 16:59:56 +0200

    Ajout de la classe Database dans le fichier Database.php du dossier src/Service

--- src/Service/Database.php
+++ src/Service/Database.php
@@ -2,3 +2,20 @@
 
 declare(strict_types=1);
 // class pour gérer la connection à la base de donnée
+class Database
+{
+    /**
+     *  Retourne une connexion à la base de données
+     *
+     * @return PDO
+     */
+    public function getPdo(): PDO
+    {
+        $pdo = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'studentBlogPOO', 'zkollf9ITvoJeupP', [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+
+        return $pdo;
+    }
+}
