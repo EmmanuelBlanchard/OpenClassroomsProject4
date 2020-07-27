141c565a9c59469ca84f5341fd3d5bde354c513b
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2020-07-27 11:45:49 +0200

    Dans dossier templates/frontoffice, fichier posts.html.php, enlevement des balises <hr>

--- templates/frontoffice/posts.html.php
+++ templates/frontoffice/posts.html.php
@@ -1,11 +1,11 @@
 <section>
     <h2>Tous les épisodes</h2>
-    <hr>
+
     <?php foreach($data['allposts'] as $post): ?>
     <p>id = <?=$post['id']?></p>
     <p>title = <?=$post['title']?></p>
     <p>introduction = <?=$post['introduction']?></p>
     <a href="index.php?action=post&id=<?=$post['id']?>\" class="linkToTheRestOfThePost">Lire la suite</a>
-    <hr>
+   
 </section>
 <?php endforeach; ?>
