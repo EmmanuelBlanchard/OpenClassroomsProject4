ec3dd3ca6507f26e3240487bfb08acc4325db6e3
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2020-07-27 11:57:28 +0200

    Dans dossier templates/frontoffice, fichier posts.html.php, ajout de la balise <article>, recuperation de la valeur 'title' et 'introduction' de

--- templates/frontoffice/posts.html.php
+++ templates/frontoffice/posts.html.php
@@ -2,10 +2,12 @@
     <h2>Tous les épisodes</h2>
 
     <?php foreach($data['allposts'] as $post): ?>
-    <p>id = <?=$post['id']?></p>
-    <p>title = <?=$post['title']?></p>
-    <p>introduction = <?=$post['introduction']?></p>
-    <a href="index.php?action=post&id=<?=$post['id']?>\" class="linkToTheRestOfThePost">Lire la suite</a>
-   
+
+    <article>
+        <h3><?=$post['title']?></h3>
+        <p><?=$post['introduction']?></p>
+        <a href="index.php?action=post&id=<?=$post['id']?>\" class="linkToTheRestOfThePost">Lire la suite</a>
+    </article>
+    
 </section>
 <?php endforeach; ?>
