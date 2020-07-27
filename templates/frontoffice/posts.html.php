ff6c4f116c781b2858d8698a9030a7630f63bc16
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2020-07-27 10:20:59 +0200

    Dans le dossier templates/frontoffice modification du fichier posts.html.php, ajout de la balise section puis changement du titre de la balise h2 par Tous les épisodes, et ajout de la class'linkToTheRestOfThePost' dans l'ancre 'Lire le post' changé en 'Lire la suite'

--- templates/frontoffice/posts.html.php
+++ templates/frontoffice/posts.html.php
@@ -1,9 +1,11 @@
-<h2>Tous les posts</h2>
-<hr>
-<?php foreach($data['allposts'] as $post): ?>
-<p>id = <?=$post['id']?></p>
-<p>title = <?=$post['title']?></p>
-<p>text = <?=$post['text']?></p>
-<a href="index.php?action=post&id=<?=$post['id']?>">Lire le post</a>
-<hr>
+<section>
+    <h2>Tous les épisodes</h2>
+    <hr>
+    <?php foreach($data['allposts'] as $post): ?>
+    <p>id = <?=$post['id']?></p>
+    <p>title = <?=$post['title']?></p>
+    <p>text = <?=$post['text']?></p>
+    <a href="index.php?action=post&id=<?=$post['id']?> class='linkToTheRestOfThePost' ">Lire la suite</a>
+    <hr>
+</section>
 <?php endforeach; ?>
