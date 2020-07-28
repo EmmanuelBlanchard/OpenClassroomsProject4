cc76f2880f448dd43c468ea2f1f7382dd96c9d54
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2020-07-28 11:02:40 +0200

    ajout de la classe 'pCreatAt'

--- templates/frontoffice/post.html.php
+++ templates/frontoffice/post.html.php
@@ -1,6 +1,5 @@
     <article>
         <h3><?=$data['onepost']['title']?></h3>
         <p><?=$data['onepost']['content']?></p>
-        <p>Écrit le <?=$data['onepost']['created_at']?></p>
+        <p class="pCreatedAt">Écrit le <?=$data['onepost']['created_at']?></p>
     </article>
-    
