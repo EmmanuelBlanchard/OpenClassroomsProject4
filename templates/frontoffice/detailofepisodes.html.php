728b11e6dad0748a5df2442a3effc96d878dd4c6
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2020-07-29 16:08:04 +0200

    enlèvement de la balise <p> Introduction de l'épisode

--- templates/frontoffice/detailofepisodes.html.php
+++ templates/frontoffice/detailofepisodes.html.php
@@ -5,7 +5,6 @@
 
     <article>
         <h3>Épisode <?=$post['title']?></h3>
-        <p class="pIntroductionToTheEpisode">Introduction de l'épisode</p>
         <p><?=$post['content']?></p>
         <p class="pCreatedAt">Écrit le <?=$post['created_at']?></p>
     </article>
