2e594fa7716cbc275fa3319b3989b7a464966842
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2020-07-29 14:48:57 +0200

    ajout de la classe pIntroductionToTheEpisode et appliqué font-weight:400;

--- templates/frontoffice/listofepisodes.html.php
+++ templates/frontoffice/listofepisodes.html.php
@@ -5,7 +5,7 @@
 
     <article>
         <h3>Épisode <?=$post['title']?></h3>
-        <p>Introduction de l'épisode</p>
+        <p class="pIntroductionToTheEpisode">Introduction de l'épisode</p>
         <p><?=$post['introduction']?></p>
         <a href="index.php?action=post&id=<?=$post['id']?>\" class="linkToTheRestOfThePost">Lire l'épisode en entier</a>
     </article>
