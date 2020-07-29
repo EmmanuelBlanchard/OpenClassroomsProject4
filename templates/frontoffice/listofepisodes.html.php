503e9cca0e5c95a79097dc89bc6ea0779b6c3ebc
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2020-07-29 14:25:09 +0200

    ajout de la balise <p> avec <?=['introduction']?>

--- templates/frontoffice/listofepisodes.html.php
+++ templates/frontoffice/listofepisodes.html.php
@@ -4,8 +4,10 @@
     <?php foreach($data['allposts'] as $post): ?>
 
     <article>
-        <h3><?=$post['title']?></h3>
-        <a href="index.php?action=post&id=<?=$post['id']?>\" class="linkToTheRestOfThePost">Lire l'épisode</a>
+        <h3>Épisode <?=$post['title']?></h3>
+        <p>Introduction de l'épisode</p>
+        <p><?=$post['introduction']?></p>
+        <a href="index.php?action=post&id=<?=$post['id']?>\" class="linkToTheRestOfThePost">Lire l'épisode en entier</a>
     </article>
     
 </section>
