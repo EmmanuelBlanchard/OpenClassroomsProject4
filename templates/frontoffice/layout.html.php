6de56def6414f9b7c10d603998557405a5c8cac7
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2020-08-03 15:47:49 +0200

    ajout de l'ongle accueil dans le fichier layout.html.php de templateq/frontoffice

--- templates/frontoffice/layout.html.php
+++ templates/frontoffice/layout.html.php
@@ -18,6 +18,7 @@
         <nav id="navBar"> 
             <ul class="menu">
               <li class="logo"><a href="index.php?action=home">Jean Forteroche</a></li>
+              <li class="item episodes"><a href="index.php?action=home">Accueil</a></li>
               <li class="item episodes"><a href="index.php?action=listofepisodes">Liste des épisodes</a></li>
               <li class="item episodes"><a href="index.php?action=detailofepisodes">Détail des épisodes</a></li>
               <li class="item button"><a href="#">Connexion</a></li>
