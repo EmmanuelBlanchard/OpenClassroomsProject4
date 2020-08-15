00ab73412b1413d87a6debd0fee49abbbd23e841
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2020-08-15 18:55:55 +0200

    suppression espacement entre balises h3, p, a

--- templates/frontoffice/home.html.php
+++ templates/frontoffice/home.html.php
@@ -4,13 +4,9 @@
     <?php foreach($data['allposts'] as $post): ?>
 
     <article>
-        
         <h3>Épisode <?=$post['title']?></h3>
-
         <p><?=$post['introduction']?></p>
-
         <p class="pCreatedAt">Publié <?=$post['date_episode_created_the']?> </p>
-        
         <a href="index.php?action=detailofepisode&id=<?=$post['id']?>\" class="linkToTheRestOfThePost">Lire la suite</a>
     </article>
     
