9f788d08495514f030282e5f9c8f465299301238
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2020-08-17 16:19:15 +0200

    aout balise h5 apres balise article dans affichage de commentaires, et mis en commentaire balise article dans la section publier un commentaire car oublige ajout balise h5

--- templates/frontoffice/detailofepisode.html.php
+++ templates/frontoffice/detailofepisode.html.php
@@ -8,26 +8,28 @@
     </article>
 </section>
 
-<!-- Probleme selection css, si veux cibler .pComments, n'affiche que le premier pas la suite des <p> n'ont pas la class -->
+<!-- Probleme selection css, si veux cibler .pComments, n'affiche que le premier, pas la suite des <p> n'ont pas la class -->
 <section class="sectionDisplayComments">
     <h4 class="sectionH4TitleComments">Commentaires</h4>
     <?php foreach($data['allcomment'] as $post): ?>
     <article>
+        <h5>Commentaire</h5> <!-- Ajout balise h5 sinon pas compatible w3c -->
         <p class="pAuthorComments"><?=$post['author']?></p>
         <p class="pDateComments"><?=$post['date_comment_created_the']?> </p>
         <p class="pComments"><?=$post['comment']?></p>
         <div class="buttonsReportReply">
-            <a href="index.php?action=reportComment&id=<?=$post['episode_id']?>\" class="linkToTheReportOfThePostComment">Signaler</a>
-            <a href="index.php?action=replyComment&id=<?=$post['episode_id']?>\" class="linkToTheReplyOfThePostComment">Répondre</a>
+            <a href="index.php?action=reportComment&id=<?=$post['episode_id']?>" class="linkToTheReportOfThePostComment">Signaler</a>
+            <a href="index.php?action=replyComment&id=<?=$post['episode_id']?>" class="linkToTheReplyOfThePostComment">Répondre</a>
         </div>
     </article>
     <?php endforeach; ?>
 </section>
 
+<!-- Suppression de la balise article car faut une balise h5 ? -->
 <section class="sectionPostcomment">
-    <h4 class="sectionH4TitlePostComment"> Publier un commentaire</h4>
-    <article>
-        <form class="commentForm" action="index.php?action=addComment&id=<?=$data['episode']['id']?>\" method="post">
+    <h4 class="sectionH4TitlePostComment"> Publier un commentaire</h4><!--
+    <article>-->
+        <form class="commentForm" action="index.php?action=addComment&id=<?=$data['episode']['id']?>" method="post">
             <p class="commentFormComment">
                 <label for="comment">Commentaire <span>(obligatoire)</span> </label>
                 <textarea name="comment" id="comment" rows="10" cols="50" required></textarea>
@@ -47,8 +49,8 @@
             <p>
                 <input type="submit" class="inputTypeSubmitPublishComment" value="Publier" />
             </p>
-        </form>
-    </article>
+        </form><!--
+    </article>-->
 </section>
 
 
