3f154e1efadda9ca361fd20ad24ed73597597158
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2020-09-21 14:24:12 +0200

    Ajout du template amdinstnewpassword.html.php pour lors du clic sur la balise span avec lien <a> et href correspondant avec l'action setNewPassword

---
+++ templates/frontoffice/adminsetnewpassword.html.php
@@ -0,0 +1,17 @@
+<section>
+    <h2>Mot de passe oublié</h2>
+
+    <form method="post" action="">
+        <div class="">
+            <label for="email">E-mail</label>
+            <input type="email" name="email" class="" />
+        </div>
+    
+        <button type="submit" class="">Renouveller le mot de passe</button>
+        
+        <p>
+            <input type="submit" class="inputTypeSubmitRenewPassword" value="Renouveller le mot de passe" />
+        </p>
+    </form>
+    
+</section>
