673ab280c22cfe20666b865ee059c0e6546c52f0
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2020-09-21 14:57:13 +0200

    Remplacement de la balise div par la balise p au ligne 5 et 8 dans le template adminsetnewpassword.html.php

--- templates/frontoffice/adminsetnewpassword.html.php
+++ templates/frontoffice/adminsetnewpassword.html.php
@@ -2,11 +2,11 @@
     <h2>Mot de passe oublié</h2>
 
     <form method="post" action="">
-        <div class="">
+        <p>
             <label for="email">E-mail</label>
             <input type="email" name="email" class="" />
-        </div>
-            
+        <p>
+
         <p>
             <input type="submit" class="inputTypeSubmitRenewPassword" value="Renouveller le mot de passe" />
         </p>
