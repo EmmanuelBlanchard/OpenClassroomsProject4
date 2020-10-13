fb5a34f52b1897ab2278bd8d485088a84fad0cd1
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2020-10-13 11:01:56 +0200

    Dans templates\backoffice\adminloginpage.html.php : suppression du contenu entre la balise section de la ligne 1 a 19, remplacement parune blaise main, div,section,h1,form.

--- templates/backoffice/adminloginpage.html.php
+++ templates/backoffice/adminloginpage.html.php
@@ -1,21 +1,21 @@
-<section class="sectionAdminLogin">
-    <h2>Accès à la page d'administration</h2>
-
-    <form method="post" action="index.php?action=login">
-        <p class="loginFormPseudo">
-            <label for="pseudo">Pseudo <span>(obligatoire)</span> </label>
-            <input id="pseudo" name="pseudo" type="text" value="" size="30" maxlength="245" required>
-        </p>
-        <p class="loginFormPassword">
-            <label for="password">Mot de passe <span>(obligatoire)</span> </label>
-            <input id="password" name="password" type="password" value="" size="30" maxlength="245" required>
-        </p>
-
-        <p>
-            <input type="submit" class="inputTypeSubmitValidateLogin" value="Se connecter" />
-        </p>
-    </form>
-
-    <a class="linkAdminLoginPageToHome" href="index.php?action=home">Retour vers le Blog de Jean Forteroche</a>
-
-</section>
+<main class="container">
+    <div class="row">
+        <section class="col-12">
+            <h1>Accès à la page d'administration</h1>
+                <form method="post" action="index.php?action=login">
+                        <div class="form-group">
+                            <label for="pseudo">Pseudo <span>(obligatoire)</span> </label>
+                            <input id="pseudo" class="form-control" name="pseudo" type="text" value="" size="30" maxlength="245">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Mot de passe <span>(obligatoire)</span> </label>
+                            <input id="password" class="form-control" name="password" type="password" value="" size="30" maxlength="245">
+                        </div>
+                        <div class="form-group">
+                            <button class="btn btn-primary">Se connecter</button>
+                        </div>
+                </fom>
+                <a class="btn btn-primary" href="index.php?action=home">Retour vers le Blog de Jean Forteroche</a>
+        </section>
+    </div>
+</main>
