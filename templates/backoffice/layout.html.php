51881a9de3c63bb255e4dbe0a122dca127723ff0
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2020-10-20 11:54:12 +0200

    Dans templates/backoffice\layout.html.php, ligne 46 a 52, ajout d un onglet "Ajouter un episode".

--- templates/backoffice/layout.html.php
+++ templates/backoffice/layout.html.php
@@ -44,6 +44,12 @@
                             </a>
                         </li>
                         <li class="has-submenu not-current-submenu menu-top menu-icon-post open-if-no-js menu-top-first">
+                            <a href="index.php?action=addEpisode" class="has-submenu not-current-submenu menu-top menu-icon-post open-if-no-js menu-top-first" aria-haspopup="true">
+                                <div class="menu-image"></div>
+                                <div class="menu-name">Ajouter un épisode</div>
+                            </a>
+                        </li>
+                        <li class="has-submenu not-current-submenu menu-top menu-icon-post open-if-no-js menu-top-first">
                             <a href="index.php?action=comments" class="has-submenu not-current-submenu menu-top menu-icon-post open-if-no-js menu-top-first" aria-haspopup="true">
                                 <div class="menu-image"></div>
                                 <div class="menu-name">Commentaires</div>
