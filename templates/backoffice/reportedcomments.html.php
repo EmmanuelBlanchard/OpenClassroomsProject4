742a03d63cb1818277824d9c61d5e8c6b51645db
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2020-10-21 11:35:43 +0200

    Dans templates\backoffice\reportedcomments.html.php, ligne 46 changement de l affichage de date au format "d/m/Y".

--- templates/backoffice/reportedcomments.html.php
+++ templates/backoffice/reportedcomments.html.php
@@ -43,7 +43,7 @@
                             <td><?=$post['post_id']?></td>
                             <td><?=$post['pseudo']?></td>
                             <td><?=$post['comment']?></td>
-                            <td><?=$post['comment_date']?></td>
+                            <td><?=date("d/m/Y", strtotime($post['comment_date']));?></td>
                             <td><a class="btn btn-primary" href="index.php?action=approveComment&id=<?=$post['id']?>">Approuver</a></td>
                             <td><a class="btn btn-primary" href="index.php?action=deleteComment&id=<?=$post['id']?>">Supprimer</a></td>
                             </td>
