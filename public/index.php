b82dc315ae286e62b27473ef16b6297d9fcc4b7b
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2020-11-09 12:00:59 +0100

    Creation classe Request.

--- public/index.php
+++ public/index.php
@@ -5,12 +5,14 @@ require_once '../vendor/autoload.php';
 
 use App\Service\Router;
 use App\Service\Http\Session;
+use App\Service\Http\Session2;
 
 $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
 
-$session = new Session();
+//$session = new Session();
+//$session = new Session2();
 
 $router = new Router();
 $router->run();
--- src/Service/Http/Request.php
+++ src/Service/Http/Request.php
@@ -2,3 +2,40 @@
 
 declare(strict_types=1);
 // class permettant la gestion des variables supers globales de php sauf $_SESSION
+class Request
+{
+    private $get;
+    private $post;
+    private $session;
+
+    public function __construct()
+    {
+        $this->get = $_GET;
+        $this->post = $_POST;
+        $this->session = $_SESSION;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGet()
+    {
+        return $this->get;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPost()
+    {
+        return $this->post;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSession()
+    {
+        return $this->session;
+    }
+}
