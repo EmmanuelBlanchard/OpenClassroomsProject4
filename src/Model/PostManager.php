9d44c5aef924b7cf36f80291da6b92eb6d6b99eb
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2020-11-09 18:06:12 +0100

    Dans PostManager.php, suppression des fonctions showAllPost, showAllPostsById, showAllPostsByIdDesc.

--- src/Model/PostManager.php
+++ src/Model/PostManager.php
@@ -30,6 +30,29 @@ class PostManager
         return $request->fetchAll();
     }
 
+    public function getListPostsPagination(int $currentPage, int $nbPostsPerPage): ?array
+    {
+        $firstPostPage=($currentPage-1)*$nbPostsPerPage;
+        $request = $this->database->prepare('SET lc_time_names = \'fr_FR\';');
+        $request->execute();
+
+        $request = $this->database->prepare('SELECT id, title, introduction, CONCAT_WS(\' \', \'le\', DAYNAME(post_date), DAY(post_date), MONTHNAME(post_date), YEAR(post_date)) AS post_date_fr, post_status FROM posts WHERE post_status=\'publish\' ORDER BY post_date ASC LIMIT :firstPostPage, :nbPostsPerPage');
+        $request->bindValue(':firstPostPage', $firstPostPage, \PDO::PARAM_INT);
+        $request->bindValue(':nbPostsPerPage', $nbPostsPerPage, \PDO::PARAM_INT);
+        $request->execute();
+        return $request->fetchAll();
+    }
+
+    public function getListEpisodesPagination(int $currentPage, int $nbEpisodesPerPage): ?array
+    {
+        $firstEpisodePage=($currentPage-1)*$nbEpisodesPerPage;
+        $request = $this->database->prepare('SELECT id, chapter, title, introduction, post_date, post_status FROM posts ORDER BY post_date ASC LIMIT :firstEpisodePage, :nbEpisodesPerPage');
+        $request->bindValue(':firstEpisodePage', $firstEpisodePage, \PDO::PARAM_INT);
+        $request->bindvalue(':nbEpisodesPerPage', $nbEpisodesPerPage, \PDO::PARAM_INT);
+        $request->execute();
+        return $request->fetchAll();
+    }
+    
     public function getPost(int $postId): ?array
     {
         $request = $this->database->prepare('SET lc_time_names = \'fr_FR\';');
@@ -58,29 +81,6 @@ class PostManager
         return $nbTotalEpisodes;
     }
 
-    public function getListPostsPagination(int $currentPage, int $nbPostsPerPage): ?array
-    {
-        $firstPostPage=($currentPage-1)*$nbPostsPerPage;
-        $request = $this->database->prepare('SET lc_time_names = \'fr_FR\';');
-        $request->execute();
-
-        $request = $this->database->prepare('SELECT id, title, introduction, CONCAT_WS(\' \', \'le\', DAYNAME(post_date), DAY(post_date), MONTHNAME(post_date), YEAR(post_date)) AS post_date_fr, post_status FROM posts WHERE post_status=\'publish\' ORDER BY post_date ASC LIMIT :firstPostPage, :nbPostsPerPage');
-        $request->bindValue(':firstPostPage', $firstPostPage, \PDO::PARAM_INT);
-        $request->bindValue(':nbPostsPerPage', $nbPostsPerPage, \PDO::PARAM_INT);
-        $request->execute();
-        return $request->fetchAll();
-    }
-
-    public function getListEpisodesPagination(int $currentPage, int $nbEpisodesPerPage): ?array
-    {
-        $firstEpisodePage=($currentPage-1)*$nbEpisodesPerPage;
-        $request = $this->database->prepare('SELECT id, chapter, title, introduction, post_date, post_status FROM posts ORDER BY post_date ASC LIMIT :firstEpisodePage, :nbEpisodesPerPage');
-        $request->bindValue(':firstEpisodePage', $firstEpisodePage, \PDO::PARAM_INT);
-        $request->bindvalue(':nbEpisodesPerPage', $nbEpisodesPerPage, \PDO::PARAM_INT);
-        $request->execute();
-        return $request->fetchAll();
-    }
-    
     public function previousPost($chapter): ?int
     {
         $request = $this->database->prepare('SELECT id FROM posts WHERE chapter = (SELECT MAX(chapter) FROM posts WHERE chapter < :chapter)');
@@ -99,27 +99,6 @@ class PostManager
 
     /*************************************************************************/
     
-    public function showAllPost()
-    {
-        $request = $this->database->prepare('SELECT * FROM posts ORDER BY post_date');
-        $request->execute();
-        return $request->fetchAll(\PDO::FETCH_ASSOC);
-    }
-
-    public function showAllPostsById()
-    {
-        $request = $this->database->prepare('SELECT * FROM posts ORDER BY id');
-        $request->execute();
-        return $request->fetchAll(\PDO::FETCH_ASSOC);
-    }
-
-    public function showAllPostsByIdDesc()
-    {
-        $request = $this->database->prepare('SELECT * FROM posts ORDER BY id DESC');
-        $request->execute();
-        return $request->fetchAll(\PDO::FETCH_ASSOC);
-    }
-
     public function showOnePost(int $postId)
     {   // Que mettre comme proprietes typées ?
         //  : ?array pour recuperer les données dans un tableau
