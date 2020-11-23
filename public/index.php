76b9613e09a9df3a61048eefc8a311134b3c6100
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2020-11-23 16:36:07 +0100

    Dans index.php, lignes 7 a 9, suppression du code inutile.

--- public/index.php
+++ public/index.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 require_once '../vendor/autoload.php';
 
 use App\Service\Router;
-use App\Service\Http\Request;
-use App\Service\Http\Session;
-use App\Service\Security\Token;
 
 $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
