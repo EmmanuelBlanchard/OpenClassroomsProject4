8b9623ea9bfdf2dc44580a1da984f19dcc854293
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2020-11-26 17:18:03 +0100

    Dans templates\frontoffice\error.html.php, lignes 6,7, suppression des balises section et h2.

--- templates/frontoffice/error.html.php
+++ templates/frontoffice/error.html.php
@@ -3,17 +3,12 @@
         <p class="pError"> Erreur : l'affichage des erreurs n'est pas possible. </p>
     </section>
 <?php elseif ($data['errortab'] !== null): ?>
-    <section>
-        <h2>Erreurs</h2>
-
-        
         <?php foreach($data['errortab'] as $post): ?>
-                <article>
-                    <h3>Erreur</h3>
-                    <p> <?=$post['errorName']?> </p>
-                    <p> <?=$post['errorValue']?> </p>
-                    <a href="index.php?action=home" class="linkToTheRestOfThePost">Accueil</a>
-                </article>
-    </section>
+            <article>
+                <h3>Erreur</h3>
+                <p> <?=$post['errorName']?> </p>
+                <p> <?=$post['errorValue']?> </p>
+                <a href="index.php?action=home" class="linkToTheRestOfThePost">Accueil</a>
+            </article>
     <?php endforeach; ?>
 <?php endif; ?>
