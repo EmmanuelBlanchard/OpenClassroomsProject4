f64f6ecffab604577f3442159242ecc3f08b7921
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2020-12-01 21:08:49 +0100

    Dans templates\backoffice\readepisodes.html.php, ligne 17-22: ajout de la condition php if ($data['sessionmessage'] \!== null).

--- templates/backoffice/readepisodes.html.php
+++ templates/backoffice/readepisodes.html.php
@@ -13,6 +13,12 @@
             <?= $data['sessionerreur'] ?>
         </div>
     <?php endif; ?>
+
+    <?php if ($data['sessionmessage'] !== null): ?>
+        <div class="alert alert-success" role="alert">
+            <?= $data['sessionmessage'] ?>
+        </div>
+    <?php endif; ?>
     
     <div class="table-responsive">
         <table class="table table-bordered table-hover table-sm">
