9dd15815c873e3566e9b765c8d2cbe743bfc3643
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2020-12-01 22:21:08 +0100

    Dans templates\backoffice\readcomments.html.php, lignes 66-69: suprression des liens vers les pages 4,5,6.

--- templates/backoffice/readcomments.html.php
+++ templates/backoffice/readcomments.html.php
@@ -63,9 +63,6 @@
             <li class="page-item"><a class="page-link" href="index.php?action=readComments&page=1">1</a></li>
             <li class="page-item"><a class="page-link" href="index.php?action=readComments&page=2">2</a></li>
             <li class="page-item"><a class="page-link" href="index.php?action=readComments&page=3">3</a></li>
-            <li class="page-item"><a class="page-link" href="index.php?action=readComments&page=4">4</a></li>
-            <li class="page-item"><a class="page-link" href="index.php?action=readComments&page=5">5</a></li>
-            <li class="page-item"><a class="page-link" href="index.php?action=readComments&page=6">6</a></li>
             <?php if ($data['nextpage'] !== null): ?>
                 <li class="page-item">
                     <a class="page-link" href="index.php?action=readComments&page=<?=$data['nextpage']?>" aria-label="Next">
