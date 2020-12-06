7b640ce8289c2e28e283dae4d9735bfe136e7e54
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2020-12-06 20:58:11 +0100

    Dans templates\backoffice\reportedcomments.html.php, ajout dun condition php id (['allreportedcomment'] === null): ?>, dans ce cas, affichage du tableau sans les balises php pour recuperer les valeurs des rangees du tabaleau. Dans AdminController.php, lignes 278-279: suppression du var_dump et die();.

--- src/Controller/Backoffice/AdminController.php
+++ src/Controller/Backoffice/AdminController.php
@@ -275,8 +275,7 @@ class AdminController
             exit();
         }
         $dataReportedComments = $this->commentManager->showAllReportedComment();
-        //var_dump($dataReportedComments);
-        //die();
+
         $this->view->render(['template' => 'reportedcomments', 'allreportedcomment' => $dataReportedComments, 'sessionmessage' => $session->getSessionMessage('message'), 'sessionerreur' => $session->getSessionMessage('erreur')], 'backoffice');
     }
 
--- templates/backoffice/reportedcomments.html.php
+++ templates/backoffice/reportedcomments.html.php
@@ -1,42 +1,85 @@
-<h2>Liste des commentaires signalés</h2>
-<?php if ($data['sessionerreur'] !== null): ?>
-        <div class="alert alert-danger" role="alert">
-            <?= $data['sessionerreur'] ?>
-        </div>
-    <?php endif; ?>
+<?php if ($data['allreportedcomment'] === null): ?>
+    <h2>Liste des commentaires signalés</h2>
+    <?php if ($data['sessionerreur'] !== null): ?>
+            <div class="alert alert-danger" role="alert">
+                <?= $data['sessionerreur'] ?>
+            </div>
+        <?php endif; ?>
 
-    <?php if ($data['sessionmessage'] !== null): ?>
-        <div class="alert alert-success" role="alert">
-            <?= $data['sessionmessage'] ?>
+        <?php if ($data['sessionmessage'] !== null): ?>
+            <div class="alert alert-success" role="alert">
+                <?= $data['sessionmessage'] ?>
+            </div>
+        <?php endif; ?>
+        <div class="table-responsive">
+            <table class="table table-bordered table-hover table-sm">
+                <thead class="text-center">
+                    <tr>
+                        <th>ID</th>
+                        <th>Episode</th>
+                        <th>Pseudo</th>
+                        <th>Commentaire</th>
+                        <th>Envoyé le</th>
+                        <th>Approuver</th>
+                        <th>Supprimer</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td data-title="ID"></td>
+                        <td data-title="Numero"></td>
+                        <td data-title="Pseudo"></td>
+                        <td data-title="Commentaire"></td>
+                        <td data-title="Envoyé le"></td>
+                        <td data-title="Approuver"></td>
+                        <td data-title="Supprimer"></td>
+                    </tr>
+                </tbody>
+                
+            </table>
+            <a class="btn btn-primary" href="index.php?action=blogControlPanel">Retour</a>
         </div>
-    <?php endif; ?>
-<div class="table-responsive">
-    <table class="table table-bordered table-hover table-sm">
-        <thead class="text-center">
-            <tr>
-                <th>ID</th>
-                <th>Episode</th>
-                <th>Pseudo</th>
-                <th>Commentaire</th>
-                <th>Envoyé le</th>
-                <th>Approuver</th>
-                <th>Supprimer</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($data['allreportedcomment'] as $post): ?>
-                <tr>
-                    <td data-title="ID"><?=$post['id']?></td>
-                    <td data-title="Numero"><?=$post['chapter']?></td>
-                    <td data-title="Pseudo"><?=$post['pseudo']?></td>
-                    <td data-title="Commentaire"><?=$post['comment']?></td>
-                    <td data-title="Envoyé le"><?=date("d/m/Y", strtotime($post['comment_date']));?></td>
-                    <td data-title="Approuver"><a class="btn btn-primary" href="index.php?action=approveComment&id=<?=$post['id']?>">Approuver</a></td>
-                    <td data-title="Supprimer"><a class="btn btn-primary" href="index.php?action=deleteComment&id=<?=$post['id']?>">Supprimer</a></td>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
-        
-    </table>
-    <a class="btn btn-primary" href="index.php?action=blogControlPanel">Retour</a>
-</div>
+<?php elseif ($data['allcommentspagination'] !== null): ?>
+    <h2>Liste des commentaires signalés</h2>
+    <?php if ($data['sessionerreur'] !== null): ?>
+            <div class="alert alert-danger" role="alert">
+                <?= $data['sessionerreur'] ?>
+            </div>
+        <?php endif; ?>
+
+        <?php if ($data['sessionmessage'] !== null): ?>
+            <div class="alert alert-success" role="alert">
+                <?= $data['sessionmessage'] ?>
+            </div>
+        <?php endif; ?>
+        <div class="table-responsive">
+            <table class="table table-bordered table-hover table-sm">
+                <thead class="text-center">
+                    <tr>
+                        <th>ID</th>
+                        <th>Episode</th>
+                        <th>Pseudo</th>
+                        <th>Commentaire</th>
+                        <th>Envoyé le</th>
+                        <th>Approuver</th>
+                        <th>Supprimer</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($data['allreportedcomment'] as $post): ?>
+                        <tr>
+                            <td data-title="ID"><?=$post['id']?></td>
+                            <td data-title="Numero"><?=$post['chapter']?></td>
+                            <td data-title="Pseudo"><?=$post['pseudo']?></td>
+                            <td data-title="Commentaire"><?=$post['comment']?></td>
+                            <td data-title="Envoyé le"><?=date("d/m/Y", strtotime($post['comment_date']));?></td>
+                            <td data-title="Approuver"><a class="btn btn-primary" href="index.php?action=approveComment&id=<?=$post['id']?>">Approuver</a></td>
+                            <td data-title="Supprimer"><a class="btn btn-primary" href="index.php?action=deleteComment&id=<?=$post['id']?>">Supprimer</a></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+                
+            </table>
+            <a class="btn btn-primary" href="index.php?action=blogControlPanel">Retour</a>
+        </div>
+<?php endif; ?>
