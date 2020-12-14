0385abb3f33d67acf353ad882caa3b66ec67a1b4
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2020-12-14 20:33:38 +0100

    Dans css\stylebackoffice.css, ligne 75: modification de la valeur de la propriete padding-left pour le selecteur td a 32% afin que le texte de commentaires dans la table Liste des commentaires en version mobile ne rejoignent pas le selecteur td avec data-title="Commentaire". Dans templates\backoffice\reportedcomments.html.php, suppression de la balise table et a la place ajout d une balise <p> Auncun Commentaire Signalé</p>.

--- templates/backoffice/reportedcomments.html.php
+++ templates/backoffice/reportedcomments.html.php
@@ -11,32 +11,8 @@
                 <?= $data['sessionmessage'] ?>
             </div>
         <?php endif; ?>
-        <div class="table-responsive">
-            <table class="table table-bordered table-hover table-sm">
-                <thead class="text-center">
-                    <tr>
-                        <th>ID</th>
-                        <th>Numéro de l'épisode</th>
-                        <th>Pseudo</th>
-                        <th>Commentaire</th>
-                        <th>Envoyé le</th>
-                        <th>Approuver</th>
-                        <th>Supprimer</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td data-title="ID"></td>
-                        <td data-title="Numéro de l'épisode"></td>
-                        <td data-title="Pseudo"></td>
-                        <td data-title="Commentaire"></td>
-                        <td data-title="Envoyé le"></td>
-                        <td data-title="Approuver"></td>
-                        <td data-title="Supprimer"></td>
-                    </tr>
-                </tbody>
-                
-            </table>
+        <div>
+            <p>Aucun Commentaire Signalé</p>
             <a class="btn btn-primary" href="index.php?action=blogControlPanel">Retour</a>
         </div>
 <?php elseif ($data['allreportedcomment'] !== null): ?>
