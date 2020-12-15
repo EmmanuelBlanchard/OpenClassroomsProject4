3d44d53718d0d7dcfa7750e777b5c6891e55c3f4
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2020-12-15 08:51:53 +0100

    Dans templates\backoffice\layout.html.php, lignes 15-16: suppression de lien vers dashboard.css inutile. lignes 109-114: suppression de code inutile.

--- templates/backoffice/layout.html.php
+++ templates/backoffice/layout.html.php
@@ -12,8 +12,6 @@
         
         <!--  Bootstrap core CSS -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-        <!--  Custom styles for this template -->
-        <link href="dashboard.css" rel="stylesheet">
         <!-- Appel de la feuille de style -->
         <link rel="stylesheet" type="text/css" href="css/stylebackoffice.css">
 
@@ -106,12 +104,6 @@
             </div>
         </div>
         
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script>window.jQuery || document.write('<script src="/docs/4.5/assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="/docs/4.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
-        <script src="dashboard.js"></script>
-
     </body>
 
 </html>
